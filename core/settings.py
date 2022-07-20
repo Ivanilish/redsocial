b8013f7f7007fb683c1b27be2fb067449ec567cf
Ivany <bstevenpz@outlook.com>
2022-07-19 20:08:09 -0500

    luthem libreria

--- core/settings.py
+++ core/settings.py
@@ -2,6 +2,7 @@ from email.policy import default
 from msilib.schema import Environment
 from pathlib import Path
 import os
+from telnetlib import AUTHENTICATION
 import environ
 
 env=environ.Env()
@@ -40,17 +41,48 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'django.contrib.humanize',
     'tailwind',
     'theme',
+
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'crispy_forms',
+    'crispy_tailwind',
+
+
     'core',
     'social',
 ]
+
+SITE_ID = 1
 
 TAILWIND_APP_NAME = 'theme'
 
 INTERNAL_IPS = [
     "127.0.0.1",
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACK = 'tailwind'
+CRISPY_TEMPLATE_PACK = 'tailwind'
+
+
+ACCOUNT_ALLOW_REGISTRATION = env.bool("DJANGO_ACCOUNT_ALLOW_REGISTRATION",True)
+ACCOUNT_AUTHENTICATION_METHOD = "username"
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_EMAIL_UNIQUE = True
+#AUTH_USER_MODEL = "accounts.User"
+ACCOUNT_LOGOUT_ON_GET = True
+ACCOUNT_EMAIL_VERIFICATION = "mandatory"
+ACCOUNT_LOGIN_ATTEMPTS_LIMIT = 3
+ACCOUNT_LOGIN_ATTEMPTS_TIMEOUT = 300
+LOGIN_REDIRECT_URL = "/"
+LOGIN_URL = "account_login"
+
+
+
 
 NPM_BIN_PATH = r"C:\Program Files\nodejs\npm.cmd"
 
